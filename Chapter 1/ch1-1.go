5ec3eb105564dc774cc821ff7119d75569d33da9
Jason Stieber <jrstieber@gmail.com>
2021-07-28 15:08:10 -0700

    Prob 1.1 completed

--- Chapter 1/ch1-1.go
+++ Chapter 1/ch1-1.go
@@ -1,6 +1,10 @@
 // Exercise 1.1 Modify the echo program to also print os.Args[0], the name of the command that invoked it.
 
-// Origional example
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 4.
+//!+
 
 package main
 
@@ -11,13 +15,12 @@ import (
 
 func main() {
 	var s, sep string
-	fmt.Println("are we seeing this?")
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
-	fmt.Printf("This is the varabile we are working with %v", os)
-	fmt.Println(s)
+	fmt.Printf(s)
+	fmt.Printf("The command that invoked os.Args is %v", os.Args[0])
 }
 
 //!-
