6b9c70c0ec072094bf61e5aa1a5b20cbf8d14052
efron licht <efron.dev@gmail.com>
2021-07-30 11:31:40 -0700

    add ch3 homework

---
+++ ch3/anagram_test.go
@@ -0,0 +1,22 @@
+package anagram
+
+import "testing"
+
+func TestAnagram(t *testing.T) {
+	type test = struct {
+		a, b string
+		want bool
+	}
+	for _, tt := range []test{
+		{"", "", true},
+		{"a", "a", true},
+		{"a", "b", false},
+		{"dog", "god", true},
+		{"dog ", "god", false},
+	} {
+		if got := Anagram(tt.a, tt.b); got != tt.want {
+			t.Fatalf("Anagram(%s, %s) should be %v, but is: %v", tt.a, tt.b, tt.want, got)
+		}
+	}
+
+}
