f45565a6a6784fee737660cc4bb5682e8712d977
Jason Stieber <jrstieber@gmail.com>
2021-08-13 16:19:25 -0700

    completed question 4.3 all test now passing

--- ch4/4.3.go
+++ ch4/4.3.go
@@ -1,23 +1,27 @@
 package ch4
 
+//import "fmt"
+
 func reverse64BitArray(a *[8]byte) {
-	for i := 0; i < len(a)/2; i++ {
+	//	fmt.Printf("old: %b ", a)
+	for i := 0; i < 4; i++ {
 		f := reverseBits(a[i])
-		l := reverseBits(a[len(a)-i-1])
-		a[i] = l
+		l := reverseBits(a[len(a)-1-i])
+		a[i], a[len(a)-1-i] = l, f
 		a[len(a)-1-i] = f
 	}
+	//	fmt.Printf("new:%b\n", a)
 }
 
 func reverseBits(b byte) byte {
 	newB := byte(0)
 	for i := 0; i < 8; i++ {
-		if b&1 == 1 {
-			newB = newB | byte(0000_0001)
-		}
-		b >>= 1
 		newB <<= 1
-
+		if b&1 == 1 {
+			newB = newB | byte(1)
+		}
+		// fmt.Printf("old: %b new: %b \n", b, newB)
+		b >>= 1
 	}
 	return newB
 }
